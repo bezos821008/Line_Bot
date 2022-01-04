def7a5dcfc1a282e80be26e5b2cc98495984de0d
bezos821008 <bezos821008@gmail.com>
2022-01-04 13:06:06 +0800

    2022-1-4

--- Line_Bot.py
+++ Line_Bot.py
@@ -1,20 +1,45 @@
-from flask import Flask
-from flask import request
+#import出現警告，要輸入路徑
+#https://stackoverflow.com/questions/65252074/import-path-to-own-script-could-not-be-resolved-pylance-reportmissingimports
+
+#ngork要放在程序包內
+#打開cmd
+#cd C:\Users\Chiang\Desktop\gitcode\Line_Bot\Line_Bot
+#ngork http port
+
+
+
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return('首頁')
+line_bot_api = LineBotApi('62ujzaljiXLyJDBr/R7YM3QHnOkn153edo0Vxt8mqRcPQJNYNgxBSP6+SVolCV2iE0UygC9VJffUINuw757YoRtbHfHfRMrO/U0cuVHoblv+LRp/KDHBgW9tnVhQhZgbTsVHf0WKqsZb21agmAa/IQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('ce53441f6aa752008763607c0ddb7d08')
 
-@app.route('/response/<string:name>')
-def response(name):
-    return("{}".format(name))
 
-@app.route('/get_test/',methods=['GET'])
-def get_test():
-    get_test_name = request.args.get('get_test_name')
-    return("{}".format(get_test_name))
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    
+    try:
+        handler.handle(body,signature)
+    except InvalidSignatureError:
+        abort(400)
+    return('OK')
+
+@handler.add(MessageEvent,message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
 
 if __name__ == '__main__':
     app.run()
